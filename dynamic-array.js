8381453475503356df3080ab5fe0aef61880e5c2
Robert Weekes <weekes.robert@gmail.com>
2024-06-24 01:00:56 -0700

    passing 5/10

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,47 +1,76 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+      this.capacity = defaultSize;
+      this.length = 0;
+      this.data = new Array(defaultSize);
   }
 
   read(index) {
-
-    // Your code here 
+      return this.data[index];
   }
 
   push(val) {
-
-    // Your code here 
+      this.data[this.length] = val;
+      this.length++;
   }
 
-
   pop() {
-
-    // Your code here 
+      if(this.length === 0) return undefined;  // edge case for empty arr
+      let popped = this.data[this.length - 1];
+      console.log(popped);
+      this.data[this.length - 1] = undefined;
+      this.length--;
+      return popped;
   }
 
   shift() {
-
-    // Your code here 
+      
   }
 
   unshift(val) {
-
-    // Your code here 
+      
   }
 
   indexOf(val) {
-
-    // Your code here 
+      
   }
 
   resize() {
 
-    // Your code here 
   }
 
 }
+
+// testing code
+let array = new DynamicArray();
+console.log(array);
+
+// array.data[0] = 3;
+// console.log(array.data[0]);
+// array.length = 1;
+// console.log(array.length);
+
+array.push(1);
+console.log(array.read(0));
+console.log(array.length);
+array.push(2);
+console.log(array.read(1));
+console.log(array.length);
+array.push(3);
+console.log(array.read(2));
+console.log(array.length);
+
+console.log(array.pop());
+// console.log(array.read(2));  // undef
+console.log(array.length);
+console.log(array.pop());
+// console.log(array.read(1));  // undef
+console.log(array.length);
+console.log(array.pop());
+// console.log(array.read(0));  // undef
+console.log(array.length);
+console.log(array.pop());
 
 
 module.exports = DynamicArray;
